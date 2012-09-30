362c6b303c2d58f7bcb84563a84268b0250c245c
truepattern <admin@truepattern.com>
2012-09-30 01:16:47 -0700

    handling nested elements in xml

--- History.md
+++ History.md
@@ -1,5 +1,6 @@
-# release 0.2.0
+# release 0.2.1
  * xml input supported
+ * nested element handling for xml added
 # release 0.1.0
  * yaml and json loaders 
  * convert between json and yaml examples added
--- examples/xml_test.js
+++ examples/xml_test.js
@@ -14,12 +14,13 @@ console.log(to.stringify(xmldoc));
 // load an xml with array
 var xmldoc1 = to.format.xml.load(__dirname + '/sample1.xml');
 
-// lets print it as json
-console.log('json for sample1.xml');
-console.log('======================');
-console.log(to.format.json.stringify(xmldoc1));
-
 // lets print it as yaml
 console.log('yaml for sample1.xml');
 console.log('======================');
 console.log(to.format.yaml.stringify(xmldoc1));
+
+var xmldoc2 = to.format.xml.load(__dirname + '/sample2.xml');
+// lets print it as json
+console.log('json for sample1.xml');
+console.log('======================');
+console.log(to.format.json.stringify(xmldoc2));
